cbe43bc026c45a5e6d609d998bfa41701b2ae046
Kirill Warp <kirill9990@gmail.com>
2018-01-02 13:45:53 +0300

    add component into app

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RgbHashFormComponent } from './rgb-hash-form/rgb-hash-form.component';
 import { RgbSeparatedFormComponent } from './rgb-separated-form/rgb-separated-form.component';
 
+const DECLARATION_LIST = [
+  RgbHashFormComponent,
+  RgbSeparatedFormComponent,
+];
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [RgbHashFormComponent, RgbSeparatedFormComponent]
+  declarations: [
+    ...DECLARATION_LIST,
+  ],
+  exports: [
+    ...DECLARATION_LIST,
+  ],
 })
 export class ComponentsModule { }
