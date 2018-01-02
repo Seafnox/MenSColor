ddea962aeb21580e43e5c113e8c8b65a93a418e4
Kirill Warp <kirill9990@gmail.com>
2018-01-02 13:40:05 +0300

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
+import { MatButtonModule, MatFormFieldModule, MatGridListModule, MatInputModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import {GlobalServicesModule} from './services/global-services.module'
-import {ComponentsModule} from './components/components.module'
+import { ComponentsModule } from './components/components.module';
+import { GlobalServicesModule } from './services/global-services.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     GlobalServicesModule,
     ComponentsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatGridListModule,
   ],
   declarations: [
     AppComponent
