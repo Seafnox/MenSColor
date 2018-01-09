4ae1aff68c50b052e5a75832bd153779fc82cd72
Kirill Warp <kirill9990@gmail.com>
2018-01-09 23:12:28 +0300

    add matcomponents

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule, MatFormFieldModule, MatGridListModule, MatInputModule, MatPrefix } from '@angular/material';
+import {
+  MatButtonModule, MatFormFieldModule, MatGridListModule, MatInputModule, MatOptionModule,
+  MatProgressSpinnerModule, MatSelectModule,
+} from '@angular/material';
 import { RgbHashFormComponent } from './rgb-hash-form/rgb-hash-form.component';
 import { RgbSeparatedFormComponent } from './rgb-separated-form/rgb-separated-form.component';
 import { ColorInformerComponent } from './color-informer/color-informer.component';
@@ -25,6 +28,9 @@ const MATERIAL_COMPONENT_MODULES = [
   MatInputModule,
   MatFormFieldModule,
   MatGridListModule,
+  MatProgressSpinnerModule,
+  MatOptionModule,
+  MatSelectModule,
 ];
 
 @NgModule({
